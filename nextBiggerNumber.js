b623f860786c7f7230cf68c9cc04313351852eb4
HuanQin <huan.qin@soprabanking.com>
2019-07-09 18:41:28 +0800

    this is next bigger number

---
+++ nextBiggerNumber.js
@@ -0,0 +1,28 @@
+function NextBiggerNumber(num) {
+    var that = num;
+    var num = num.toString().split('');
+    var len = num.length;
+    var temp = null;
+    var arr = [];
+    for(var i = 0;i < len; i++) {
+        for(var j = 0;j < len-1;j++) {
+            temp = num[j];
+            num[j] = num[j + 1];
+            num[j + 1] = temp;
+            arr.push(num.join(''));
+        }
+    }
+    var newArr = arr.filter(function(item, index) {
+        return item > that;
+    });
+    if(newArr.length !== 0) {
+        newArr.sort(function(a, b) {
+            return a - b;
+        });
+        return newArr[0];
+    }else {
+        return (that + ' is already the biggest !');
+    }
+}
+var target = NextBiggerNumber(321);
+console.log(target);
