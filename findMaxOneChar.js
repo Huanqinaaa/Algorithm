f7e2f5acb7042b14b662a948ab782bc96c29ad24
HuanQin <huan.qin@soprabanking.com>
2019-07-10 10:17:41 +0800

    find max one char in one char string

---
+++ findMaxOneChar.js
@@ -0,0 +1,22 @@
+// How to find out the most duplicated char in one character string ?
+var str = "hfhfgffsdfsdfsdfsdfsdfsdfsdfsdf";
+function getMaxCountAndChar(str) {
+    var Obj = {};
+    for (var i = 0; i < str.length; i++) {
+        if (Obj[str.charAt(i)]) {
+            Obj[str.charAt(i)]++;
+        } else {
+            Obj[str.charAt(i)] = 1;
+        }
+    }
+    var num = 0;
+    var char = "";
+    for (var key in Obj) {
+        if (Obj[key] > num) {
+            num = Obj[key];
+            char = key;
+        }
+    }
+    console.log("出现最多次数的是:" + char + ",出现了:" + num + "次")
+}
+getMaxCountAndChar(str);
