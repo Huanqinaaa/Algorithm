0d8664bd0e4478dc0554bd65b031b14217d4d228
HuanQin <huan.qin@soprabanking.com>
2019-07-15 11:19:49 +0800

    git add .

--- getSumNumber.js
+++ getSumNumber.js
@@ -1,2 +1,22 @@
 // Given one number, find out two numbers that their sum is the given number in an Array . 
-// 1. sort ; 2. front-back add.
+var hasNum = function ( a, target ) {   
+    let result = [];
+    var a = a.sort(function(a, b) {
+        return a -b;
+    });
+    let i = 0, j = a.length - 1;   
+    while (i < j) {      
+      if (a[i] + a[j] > target) {
+         j--;      
+      }else if(a[i] + a[j] < target) {
+         i++;
+      } else {
+        result.push(i + ':' + a[i]);
+        result.push(j + ':' + a[j]);
+        return result;
+      }      
+    }
+    return 'There is no target numbers ! ';
+  };
+  
+  console.log(hasNum([1,5,7,3,56,454,34,23], 35));
